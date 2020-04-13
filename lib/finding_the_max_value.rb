7c52a4976068037b9d2b13889943c52644a953fe
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-13 06:46:22 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,12 +1,16 @@
 # https://learn.co/tracks/fswd-prework-2-01/fswd-prework-2-01-fswd-prework/fswd-prework-2-01-fswd-prework-programming-as-conversation-part-4-collection-data-types-array-and-hash/finding-the-maximum-value-in-an-array-lab
 def find_max_value(array)
-  i = 0
-  max_value = -1
-  while i < array.length do
-    if max_value < array[i]
-      max_value = array[i]
-    end
-    i += 1
-  end
-  max_value
+array.max
 end
+
+# def find_max_value(array)
+#   i = 0
+#   max_value = -1
+#   while i < array.length do
+#     if max_value < array[i]
+#       max_value = array[i]
+#     end
+#     i += 1
+#   end
+#   max_value
+# end
